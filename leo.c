ff5817eff91a03b14f68e9371b5e8d92d5e01a29
Robson Goncalves <robson.goncalves@adp.com>
2019-03-17 15:52:18 -0300

    modified:   leo.c

--- leo.c
+++ leo.c
@@ -4,64 +4,113 @@
 
 char mostraMenu(){
   char resposta;
-  printf("##### MENU ##### \n");
-  printf("Este programa tem como finalidade a Verificação e a Validação de Expressões Proposicionais. \n\n");
-  printf("Gostaria de saber quais preposições podem ser usadas? S ou N \n");
-  printf("Para encerrar digite x \n");
-  printf("################ \n");
+  printf("############################# MENU ###################################### \n");
+  printf("#                                                                       # \n");
+  printf("# Este programa tem como finalidade a Verificação                       # \n");
+  printf("# E a Validação de Expressões Proposicionais.                           # \n");
+  printf("#                                                                       # \n");
+  printf("# Gostaria de saber quais preposições podem ser usadas? Digite S ou N   # \n");
+  printf("#                                                                       # \n");
+  printf("# Para encerrar digite x                                                # \n");
+  printf("#                                                                       # \n");
+  printf("######################################################################### \n");
   resposta = getchar();
   getchar();
   return resposta;
 }
 
 void mostraProposicoes(){
-  printf("_____________\n\n");
-  printf("Use ~ para representar a negação (~).\n");
-  printf("Use & para representar a conjunção (∧).\n");
-  printf("Use | para representar a disjunção (∨).\n");
-  printf("Use > para representar a implicação (→)\n");
-  printf("Use <> para representar a bi-implicação (↔).\n");
-  printf("_____________\n\n\n\n");
+  printf("____________________________________________________\n\n");
+  printf("# Use ~ para representar a negação (~)                \n");
+  printf("# Use & para representar a conjunção (∧)              \n");
+  printf("# Use | para representar a disjunção (∨)              \n");
+  printf("# Use > para representar a implicação (→)             \n");
+  printf("# Use <> para representar a bi-implicação (↔)         \n");
+  printf("____________________________________________________\n\n\n\n");
 }
 
 void testaProposicao(char *proposicao){
   char opcoes[] = {'~','<>','>'};
-  int j, tamanhoOpcoes;
+  char variaveis[] = {'p','q','r','s'};
+  char anterior, tipo_anterior;
+  int j, i, tamanhoOpcoes, tamanhoProposicao, counter;
 
-  printf("Lendo Proposicao: %s \n", proposicao);
+  counter=0;
+  //printf("ok 1 \n");
+
+
+
+  tamanhoProposicao = sizeof(proposicao);
+  // printf("Lendo Proposicao: %s \n", proposicao);
+
+  //for(int i=0;i<tamanhoProposicao;i++){
+  //  printf("# Lendo Proposicao: %c \n", proposicao[i]);
+  //}
+
+
   tamanhoOpcoes = sizeof(opcoes);
 
-  printf("Tamanhos %i \n\n", tamanhoOpcoes);
-  printf("_____________\n\n");
+
+
+  //printf("_____________\n\n");
+  //printf("Tamanho Opcoes %d \n\n", tamanhoOpcoes);
+  //printf("Tamanho Proposicao %d \n\n", tamanhoProposicao);
+  //printf("_____________\n\n");
+
+
+
 
   for(int j=0;j<tamanhoOpcoes;j++){
+    //printf("ok 2 \n");
+    //printf("Indice: [%d] \n", j);
+
+    //printf("********************\n\n");
+    //printf("Proposicao %c \n", proposicao[0]);
+    //printf("Opcao %c \n", opcoes[j]);
+    //printf("********************\n\n");
 
 
-    printf("********************\n\n");
-    printf("Proposicao %c \n", proposicao[0]);
-    printf("Opcao %c \n", opcoes[j]);
-    printf("********************\n\n");
+    anterior = proposicao[0];
 
+    if (proposicao[0] != "\0"){
+      if(opcoes[j] == proposicao[0]){
+        //printf("ok 3 \n");
+        counter = counter + 1;
+        //printf("ok 4 \n");
+        //printf("Proposicao Valida! \n\n");
+        //return;
+        //return anterior;
+      }
 
-    if(opcoes[j] == proposicao[0]){
-    // if(!strcmp(opcoes[j], proposicao)){
-      printf("Proposicao Valida! \n\n");
-      return;
+      // else {
+      //   printf("Proposicao Invalida! \n\n");
+      // }
+      //return anterior;
     }
-    else {
-      printf("Proposicao Invalida! \n\n");
-    }
-    // printf("[%d]: \n", j);
-  //  return
   }
+
+
+
+
+  if (counter == 0 ){
+    //printf("ok 5 \n");
+    printf("# [!!!] Proposicao  %c  Invalida! \n", anterior);
+  }
+  else {
+    printf("# [!] Proposicao Valida! \n\n");
+    //printf("ok 6 \n");
+  }
+  //printf("ok 7 \n");
+
 }
 
 char lerProposicao(){
   char * proposicao[61];
-  printf("Insira a proposição para validar: \n");
+  printf("# Insira a proposição para validar: \n");
   scanf("%s", &proposicao);
-  // gets(proposicao);
-  printf("Proposicao: %c \n", proposicao[0]);
+
+
+  //printf("### Iniciando teste Proposicao: %c \n", proposicao[0]);
   testaProposicao(proposicao);
 }
 
@@ -72,10 +121,8 @@ void main () {
   char resposta;
   do {
     resposta = mostraMenu();
-    // resposta = getchar();
-    // getchar();
     if(resposta == 'x'){
-      printf("Encerrando em 3 segundos... \n");
+      printf("# Encerrando em 3 segundos... \n");
       sleep(3);
       exit(0);
     }
@@ -85,7 +132,7 @@ void main () {
     else if (resposta == 'N' || resposta == 'n') {
       lerProposicao();
     }
-    printf("Limpando o console em 5 segundos... \n");
+    printf("# Limpando o console em 5 segundos... \n");
     sleep(5);
     system("clear");
   } while (resposta != 'x');
